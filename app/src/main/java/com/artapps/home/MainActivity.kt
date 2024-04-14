b74ea47c30638ae052ff2df8feae93781d018a91
art3mka3 <167025587+art3mka3@users.noreply.github.com>
2024-04-14 23:12:22 +0300

    Смена текста в коде MainActivity.kt

--- app/src/main/java/com/artapps/home/MainActivity.kt
+++ app/src/main/java/com/artapps/home/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.artapps.home
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -16,5 +18,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = "Новый текст"
+
     }
 }
